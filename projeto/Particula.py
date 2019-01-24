534f0026b9ab83ec4dbe589916af659bd7c98783
Felipe017 <43702472+Felipe017@users.noreply.github.com>
2019-01-23 23:19:38 -0200

    Update Particula.py

--- projeto/Particula.py
+++ projeto/Particula.py
@@ -1,24 +1,24 @@
 class Particula:
-    def __init__(self, px = 0, py = 0):
+    def __init__(self, px = 0, py = 0, m = 0):
             self.px = float(px) 
             self.py = float(py)
-         
+            self.m = float(m)
     def energia(self):
-        # E^2 = m^2 + P^2, onde P = sqrt(px^2 + py^2). a massa do elétron é de 0.511 MeV
-        return (((0.511)**2 + self.px**2 + self.py**2)**0.5)
+        # E^2 = m^2 + P^2, onde P = sqrt(px^2 + py^2)
+        return (((self.m)**2 + self.px**2 + self.py**2)**0.5)
     
     def tri_vet(self):
         tri_vet = [self.px, self.py, self.energia()] 
         return tri_vet
 
-class eletron(Particula):
+class eletron(Particula): 
     """ Classe que representa o eletron, que é um tipo particula"""
-    def __init__(self, px = 0, py = 0):
-        super().__init__(px,py)
+    def __init__(self, px = 0, py = 0, m = 0.511):#a massa do elétron é de 0.511 MeV
+        super().__init__(px,py,m)
 
 class foton(Particula):
     """ Classe que representa o foton, que é um tipo particula"""
-    def __init__(self, px = 0, py = 0):
+    def __init__(self, px = 0, py = 0, m = 0):
         #Forma de chamar o inicializador da classe basse
-        super().__init__(px,py)
+        super().__init__(px,py,m)
         
