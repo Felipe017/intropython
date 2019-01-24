68cd76a5f38680d05b370533c34de44cd5ab8d0c
Felipe017 <43702472+Felipe017@users.noreply.github.com>
2019-01-23 22:44:00 -0200

    Update Particula.py

--- projeto/Particula.py
+++ projeto/Particula.py
@@ -1,16 +1,24 @@
-class  Particula :
-   
-    def  __init__ ( self ,  m = 0, p = 0): 
-       
-        self.m = float(m)
-        self.p = float(p) 
+class Particula:
+    def __init__(self, px = 0, py = 0):
+            self.px = float(px) 
+            self.py = float(py)
+         
+    def energia(self):
+        # E^2 = m^2 + P^2, onde P = sqrt(px^2 + py^2). a massa do elétron é de 0.511 MeV
+        return (((0.511)**2 + self.px**2 + self.py**2)**0.5)
+    
+    def tri_vet(self):
+        tri_vet = [self.px, self.py, self.energia()] 
+        return tri_vet
 
-    def energia (self):
+class eletron(Particula):
+    """ Classe que representa o eletron, que é um tipo particula"""
+    def __init__(self, px = 0, py = 0):
+        super().__init__(px,py)
 
-	#E=((m*c**2)**2+(p*c)**2)**0.5
-        return ((self.m)**2+(self.p)**2)**0.5
-
-    def quad_vet (self):
-        quad_vet= [self.p, self.energia()] 
-        return quad_vet 
-
+class foton(Particula):
+    """ Classe que representa o foton, que é um tipo particula"""
+    def __init__(self, px = 0, py = 0):
+        #Forma de chamar o inicializador da classe basse
+        super().__init__(px,py)
+        
